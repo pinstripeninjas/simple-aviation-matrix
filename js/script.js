82fb48302964caf997864e57a780b647552945a4
pinstripeninjas <pinstripeninjas@gmail.com>
2020-04-21 11:27:29 -0700

    added colors to matrix, still fixing formatting

--- js/script.js
+++ js/script.js
@@ -67,21 +67,49 @@ function buildMatrixContent(forecast, criteria) {
 		fieldNameDiv.classList.add("field-name");
 		finalDiv.append(fieldNameDiv);
 		// map through values and add to matrix
-		field.values.map((value) => {
+		field.values.map((value, j) => {
 			const valueDiv = document.createElement("div");
-			valueDiv.classList.add("forecast-item");
+			valueDiv.classList.add("forecast-item", applyColor(value, field, criteria.body[i], j));
 			valueDiv.innerText = value;
+			// check value with criteria and apply color class
+
 			return finalDiv.append(valueDiv);
 		});
 		// map through criteria values and add to matrix
-		criteria.body[i].textArray.map((criteria) => {
+		criteria.body[i].textArray.map((criteriaText, j) => {
 			const criteriaDiv = document.createElement("div");
-			criteriaDiv.classList.add("criteria-item");
-			criteriaDiv.innerText = criteria;
+			criteriaDiv.classList.add("criteria-item", criteria.body[i].colorsClass[j]);
+			criteriaDiv.innerText = criteriaText;
 			return finalDiv.append(criteriaDiv);
 		});
 		return matrixContent.append(finalDiv);
 	});
+}
+
+function applyColor(value, field, criteria, j) {
+	// check if textbox to apply value comparison
+	if (field.dataType === "textbox") {
+		// check if variable is sfc wind to remove directional component
+		if (field.variable === "maxSfcWind") {
+			value = Number(value.slice(2));
+		}
+		// return color for associated class
+		if (value > criteria.yellow) {
+			return "red";
+		} else if (value > criteria.green) {
+			return "yellow";
+		} else {
+			return "green";
+		}
+	} else if (field.dataType === "dropdown") {
+		if (field.category[j] === 2) {
+			return "red";
+		} else if (field.category[j] === 1) {
+			return "yellow";
+		} else {
+			return "green";
+		}
+	}
 }
 
 getDataForMatrix();
