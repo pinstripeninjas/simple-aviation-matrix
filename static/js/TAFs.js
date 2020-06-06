854bc352be5593128eb94690b51e7855a7e62ddf
pinstripeninjas <pinstripeninjas@gmail.com>
2020-06-06 14:43:52 -0700

    testing discussion

--- static/js/TAFs.js
+++ static/js/TAFs.js
@@ -1,12 +1,19 @@
 // URL locations
 const proxyUrl = "https://cors-anywhere.herokuapp.com/";
 const targetUrl = "https://www.aviationweather.gov/cgi-bin/json/TafJSON.php?bbox=-112,31,-109,33";
+const discussionUrl = "https://api.weather.gov/products/types/AFD/locations/TWC";
 
 const allTAFs = document.querySelector("#TAFs");
+const discussion = document.querySelector("#discussion");
 const loader = document.querySelector(".loader");
 const changeGroup = ["BECMG", "TEMPO", "FM", "PROB"];
 
 getTAFs();
+getDiscussion();
+
+function hideLoader() {
+	loader.classList.add("display-none");
+}
 
 function getTAFs() {
 	fetch(proxyUrl + targetUrl)
@@ -63,27 +70,16 @@ function adjustAlignment(rawTAF) {
 	return finalArray;
 }
 
-function hideLoader() {
-	loader.classList.add("display-none");
+async function getDiscussion() {
+	const res = await fetch(discussionUrl);
+	const data = await res.json();
+	const res2 = await fetch(data["@graph"][0]["@id"]);
+	const data2 = await res2.json();
+	const header = document.createElement("h2");
+	const body = document.createElement("p");
+	header.innerText = "Aviation Discussion";
+	body.innerText = data2.productText;
+	discussion.append(header, body);
 }
 
-// function test(TAF) {
-// 	const splitArray = rawTAF.split(" ");
-// 	const finalArray = [];
-// 	let sliceIndex = 0;
-// 	// loop through array and check if contains changeGroup keywords
-// 	for (let i = 0; i < splitArray.length; i++) {
-// 		// loop through changeGroup to see if this index includes a keyword
-// 		for (let change of changeGroup) {
-// 			const check = splitArray[i].includes(change);
-// 			if (check) {
-// 				const tempArray = splitArray.slice(sliceIndex, i);
-// 				finalArray.push(tempArray);
-// 				sliceIndex = i;
-// 			}
-// 		}
-// 	}
-// 	// Since no more changeGroup, must add final TAF line
-// 	finalArray.push(splitArray.slice(sliceIndex, splitArray.length));
-// 	return finalArray;
-// }
+getDiscussion();
